b15cbf5257cb2d3273c83f3a0b33478dfde6843e
Guillaume Monnet <255kb@users.noreply.github.com>
2016-11-18 22:43:26 +0100

    display saved routes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AddPage } from '../add/add';
+import { RouteProvider } from '../../providers/route';
+import { IRoute } from '../../interfaces/IRoute';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private routes: Array<IRoute> = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private routeProvider: RouteProvider) {
+    routeProvider.getRoutes().then((storedRoutes) => {
+      this.routes = storedRoutes;
+    }).catch(() => {
 
+    });
   }
 
   addFavoriteRoute() {
