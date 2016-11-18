307115b4ace6ba039a64a235d12e0092a1b5de03
Guillaume Monnet <255kb@users.noreply.github.com>
2016-11-18 21:51:55 +0100

    Add storage provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { AddPage } from '../pages/add/add';
 import { HomePage } from '../pages/home/home';
@@ -21,7 +22,8 @@ import { BikeProvider } from '../providers/bike';
     HomePage
   ],
   providers: [
-    BikeProvider
+    BikeProvider,
+    Storage
   ]
 })
 export class AppModule {}
