0e9955fd6132a57739de7cbf080e7fa55cf0bae7
Guillaume Monnet <255kb@users.noreply.github.com>
2016-12-05 21:24:33 +0100

    Update regex for some special stations with weird name

--- src/pipes/stationName.ts
+++ src/pipes/stationName.ts
@@ -6,7 +6,7 @@ import { Injectable, Pipe, PipeTransform } from '@angular/core';
 @Injectable()
 export class StationName implements PipeTransform {
   public transform(station: any, showNumber: boolean): string {
-    let nameWithoutNumber = station.name.replace(/[0-9]+ *- */, '');
+    let nameWithoutNumber = station.name.replace(/[a-z0-9]+ *- */gi, '');
     if (showNumber) {
       return `${nameWithoutNumber} (no. ${station.number})`;
     } else {
