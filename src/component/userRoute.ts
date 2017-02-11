12435e8969eeae39ac3123c06324016e9d9bc530
Guillaume Monnet <255kb@users.noreply.github.com>
2017-02-11 15:02:41 +0100

    Add card border color

--- src/component/userRoute.ts
+++ src/component/userRoute.ts
@@ -1,22 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { IRoute } from '../interfaces/IRoute';
 import { RouteProvider } from '../providers/route';
 
 //correct orange color not showing
 @Component({
   selector: 'user-route',
-  templateUrl: 'userRoute.html',
-  styles: [
-    '.spacing-right50 {display:inline-block;min-width:50px;}',
-    '.spacing-right20 {display:inline-block;min-width:20px;}',
-    '.bold {font-weight:bold;}'
-  ]
+  templateUrl: 'userRoute.html'
 })
-export class UserRouteComponent {
+export class UserRouteComponent implements OnChanges {
   @Input() route: IRoute;
   @Input() routeIndex: number;
 
   public awaitingDeletion: boolean = false;
+  public cardBorder: Array<string>;
 
   constructor(private routeProvider: RouteProvider) {
   }
@@ -29,6 +25,22 @@ export class UserRouteComponent {
       setTimeout(() => {
         this.awaitingDeletion = false;
       }, 3000);
+    }
+  }
+
+  ngOnChanges(changes) {
+    if (changes.route && changes.route.currentValue) {
+      let routeNewValue = changes.route.currentValue;
+
+      if (routeNewValue.startStation.data && routeNewValue.endStation.data) {
+        if (routeNewValue.startStation.data.color === 'green' && routeNewValue.endStation.data.color === 'green') {
+          this.cardBorder = ['border-green'];
+        } else if (routeNewValue.startStation.data.color === 'orange' || routeNewValue.endStation.data.color === 'orange') {
+          this.cardBorder = ['border-orange'];
+        } else if (routeNewValue.startStation.data.color === 'red' || routeNewValue.endStation.data.color === 'red') {
+          this.cardBorder = ['border-red'];
+        }
+      }
     }
   }
 }
