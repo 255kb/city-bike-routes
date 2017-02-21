77859419255dc543219e05c6b25882e01ee87460
Guillaume Monnet <255kb@users.noreply.github.com>
2017-02-21 22:31:36 +0100

    implements ngdocheck to update UI depending on station info

--- src/component/userRoute.ts
+++ src/component/userRoute.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, DoCheck, KeyValueDiffers } from '@angular/core';
 import { IRoute } from '../interfaces/IRoute';
 import { RouteProvider } from '../providers/route';
 
@@ -7,14 +7,21 @@ import { RouteProvider } from '../providers/route';
   selector: 'user-route',
   templateUrl: 'userRoute.html'
 })
-export class UserRouteComponent implements OnChanges {
+export class UserRouteComponent implements DoCheck {
   @Input() route: IRoute;
   @Input() routeIndex: number;
 
   public awaitingDeletion: boolean = false;
   public cardBorder: Array<string>;
+  private startStationDiffer: any;
+  private endStationDiffer: any;
 
-  constructor(private routeProvider: RouteProvider) {
+  constructor(private routeProvider: RouteProvider, private differs: KeyValueDiffers) {
+  }
+
+  ngOnInit() {
+    this.startStationDiffer = this.differs.find(this.route.startStation.data).create(null);
+    this.endStationDiffer = this.differs.find(this.route.endStation.data).create(null);
   }
 
   public removeRoute() {
@@ -28,19 +35,35 @@ export class UserRouteComponent implements OnChanges {
     }
   }
 
-  ngOnChanges(changes) {
-    if (changes.route && changes.route.currentValue) {
-      let routeNewValue = changes.route.currentValue;
+  ngDoCheck() {
+    let startStationChanges = this.startStationDiffer.diff(this.route.startStation.data);
+    let endStationChanges = this.endStationDiffer.diff(this.route.endStation.data);
+    let leastColor = 'green';
+    let hasChanged = false;
 
-      if (routeNewValue.startStation.data && routeNewValue.endStation.data) {
-        if (routeNewValue.startStation.data.color === 'green' && routeNewValue.endStation.data.color === 'green') {
-          this.cardBorder = ['border-green'];
-        } else if (routeNewValue.startStation.data.color === 'orange' || routeNewValue.endStation.data.color === 'orange') {
-          this.cardBorder = ['border-orange'];
-        } else if (routeNewValue.startStation.data.color === 'red' || routeNewValue.endStation.data.color === 'red') {
-          this.cardBorder = ['border-red'];
+    let changeCallback = (changes) => {
+      if (changes.key === 'color' && changes.currentValue) {
+        if (changes.currentValue === 'orange') {
+          leastColor = changes.currentValue;
         }
+        if (changes.currentValue === 'red') {
+          leastColor = changes.currentValue;
+        }
+        hasChanged = true;
       }
+    };
+
+    if (startStationChanges) {
+      startStationChanges.forEachChangedItem(changeCallback);
+      startStationChanges.forEachAddedItem(changeCallback);
+    }
+    if (endStationChanges) {
+      endStationChanges.forEachChangedItem(changeCallback);
+      endStationChanges.forEachAddedItem(changeCallback);
+    }
+
+    if (hasChanged) {
+      this.cardBorder = ['border-' + leastColor];
     }
   }
 }
